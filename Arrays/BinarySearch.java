d49b68fcef09d4a4f8452dc003d7ec99c8588c56
Your Name <you@example.com>
2022-03-26 20:14:53 +0530

    update

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,26 @@
+// Returning index value if element is present in array otherwise return -1
+
+
+public class Solution{
+    public int search(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left)/2;
+
+            if(arr[mid] < target){
+                left = mid + 1;
+            }
+            else if(arr[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
